2060a29c34b5a33e6ec278b020a996d62b8adad6
Li Ma <li.ma@duke.edu>
2017-08-17 16:17:26 -0400

    plot only windows at given Bayesian FDR level.

---
+++ R/plot1D_sig_windows.R
@@ -0,0 +1,18 @@
+plot1D_sig_windows = function(ans_mrs,fdr=0.1,precision = 0.001,...) { # Plot windows significant at given Bayesian FDR level
+  ans_mrs2 = ans_mrs
+  c_vec = seq(0,1,by=precision)
+  FDR = double(length(c_vec))
+  for (i in 1:length(c_vec)) {
+    FDR[i] = bFDR(ans_mrs2,c_vec[i])
+  }
+  FDR[length(c_vec)] = 0
+  c = c_vec[min(which(FDR<fdr))]
+  print(c)
+  sig_window_ind = which(ans_mrs2$RepresentativeTree$AltProbs > c)
+  print(sig_window_ind)
+  n_windows = length(ans_mrs2$RepresentativeTree$AltProbs)
+  ans_mrs2$RepresentativeTree$AltProbs[setdiff(1:n_windows,sig_window_ind)] = 0
+  ans_mrs2$RepresentativeTree$EffectSizes[setdiff(1:n_windows,sig_window_ind)] = 0
+  
+  plot1D(ans_mrs2,...)
+}
